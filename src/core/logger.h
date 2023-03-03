5ffe980968d75d87211b02a85841923c52eb429b
nilesh <nilesh@pop-os.localdomain>
2023-03-03 21:22:20 +0530

    ifdk why i am doin this

--- src/core/logger.h
+++ src/core/logger.h
@@ -25,3 +25,5 @@ b8 initialize_logging();
 void shutdown_logging();
 
 KAPI void log_output(log_level level, const char* message, ...);
+
+#define KFATAL(message, ...) log_output(LOG_LEVEL_FATAL, message, #__VA_ARGS__);
